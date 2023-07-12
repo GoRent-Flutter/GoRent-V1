076622425d21c9784876ea28c76a66b937072ef2
EmanMofeed <khalaf.eman2000@gmail.com>
2023-07-12 23:58:34 +0300

    Owner apartments

--- lib/screens/owner_view/owner_apartments.dart
+++ lib/screens/owner_view/owner_apartments.dart
@@ -1,48 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
+import '../../constraints.dart';
 import 'edit_apartment_screen.dart';
 
-class apartment {
+class Apartment {
   final String city;
   final String type;
   final int price;
   final String address1;
 
-  apartment({
+  Apartment({
     required this.city,
     required this.type,
     required this.price,
     required this.address1,
   });
-}
-
-class Myapartment extends apartment {
-  final String city;
-  final String type;
-  final int price;
-  final String address1;
-
-  Myapartment({
-    required this.city,
-    required this.type,
-    required this.price,
-    required this.address1,
-  }) : super(city: city, type: type, price: price, address1: address1);
-}
-
-class Myapartment2 extends apartment {
-  final String city;
-  final String type;
-  final int price;
-  final String address1;
-
-  Myapartment2({
-    required this.city,
-    required this.type,
-    required this.price,
-    required this.address1,
-  }) : super(city: city, type: type, price: price, address1: address1);
 }
 
 class OwnerApartmentsScreen extends StatefulWidget {
@@ -57,9 +30,7 @@ class OwnerApartmentsScreen extends StatefulWidget {
 class _OwnerApartmentsScreenState extends State<OwnerApartmentsScreen> {
   late DatabaseReference _databaseRef;
   late DatabaseReference _databaseRef2;
-  late DatabaseReference _databaseRefs;
-  List<Myapartment> _Myapartment = [];
-  List<Myapartment2> _Myapartment2 = [];
+  List<Apartment> _apartments = [];
 
   @override
   void initState() {
@@ -67,134 +38,146 @@ class _OwnerApartmentsScreenState extends State<OwnerApartmentsScreen> {
     _databaseRef = FirebaseDatabase.instance.reference().child('rent');
     _databaseRef2 = FirebaseDatabase.instance.reference().child('sale');
 
-    _databaseRef.onValue.listen((event) {
-      if (event.snapshot.value != null) {
-        final rentmyapartment = (event.snapshot.value as Map<dynamic, dynamic>)
-            .cast<String, dynamic>();
-        setState(() {
-          _Myapartment = rentmyapartment.entries
-              .where((entry) => entry.value['OwnerID'] == widget.ownerId)
-              .map((entry) {
-            final rentedapartment = Map<String, dynamic>.from(entry.value);
-            return Myapartment(
-              city: rentedapartment['city'] as String,
-              type: rentedapartment['type'] as String,
-              price: rentedapartment['price'] as int,
-              address1: rentedapartment['address1'] as String,
-            );
-          }).toList();
-        });
-      }
-    });
-    _databaseRef2.onValue.listen((event) {
-      if (event.snapshot.value != null) {
-        final salemyapartment = (event.snapshot.value as Map<dynamic, dynamic>)
-            .cast<String, dynamic>();
-        setState(() {
-          _Myapartment2 = salemyapartment.entries
-              .where((entry) => entry.value['OwnerID'] == widget.ownerId)
-              .map((entry) {
-            final saleapartment = Map<String, dynamic>.from(entry.value);
-            return Myapartment2(
-              city: saleapartment['city'] as String,
-              type: saleapartment['type'] as String,
-              price: saleapartment['price'] as int,
-              address1: saleapartment['address1'] as String,
-            );
-          }).toList();
-        });
-      }
-    });
+    _fetchApartments();
   }
 
-  List<apartment> _combineApartments() {
-    List<apartment> combinedList = [];
-    combinedList.addAll(_Myapartment);
-    combinedList.addAll(_Myapartment2);
-    return combinedList;
+  void _fetchApartments() {
+    _databaseRef.onValue.listen((event) {
+      if (event.snapshot.value != null) {
+        final rentApartments = (event.snapshot.value as Map<dynamic, dynamic>)
+            .cast<String, dynamic>();
+        setState(() {
+          _apartments = rentApartments.entries
+              .where((entry) => entry.value['OwnerID'] == widget.ownerId)
+              .map((entry) {
+            final rentedApartment = Map<String, dynamic>.from(entry.value);
+            return Apartment(
+              city: rentedApartment['city'] as String,
+              type: rentedApartment['type'] as String,
+              price: rentedApartment['price'] as int,
+              address1: rentedApartment['address1'] as String,
+            );
+          }).toList();
+        });
+      }
+    });
+
+    _databaseRef2.onValue.listen((event) {
+      if (event.snapshot.value != null) {
+        final saleApartments = (event.snapshot.value as Map<dynamic, dynamic>)
+            .cast<String, dynamic>();
+        setState(() {
+          _apartments.addAll(saleApartments.entries
+              .where((entry) => entry.value['OwnerID'] == widget.ownerId)
+              .map((entry) {
+            final saleApartment = Map<String, dynamic>.from(entry.value);
+            return Apartment(
+              city: saleApartment['city'] as String,
+              type: saleApartment['type'] as String,
+              price: saleApartment['price'] as int,
+              address1: saleApartment['address1'] as String,
+            );
+          }));
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('apartments'),
+        title: Text('الشقق'),
+        centerTitle: true,
+        backgroundColor: primaryRed,
       ),
-      body: ListView.builder(
-        itemCount: _combineApartments().length,
-        itemBuilder: (BuildContext context, int index) {
-          final apart = _combineApartments()[index];
-          return Card(
-            elevation: 2,
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              title: Text(
-                'Apartment City: ${apart.city}',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 8),
-                  Text(
-                    'type: ${apart.type}',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'address: ${apart.address1}',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    'price: ${apart.price}',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ],
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('Confirm Delete'),
-                            content: Text(
-                                'Are you sure you want to delete this apartment?'),
-                            actions: [
-                              TextButton(
-                                child: Text('Cancel'),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
+      body: Container(
+        color: Colors.white,
+        child: _apartments.isEmpty
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: _apartments.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final apartment = _apartments[index];
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey[300]!),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            ' المدينة: ${apartment.city}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'العنوان: ${apartment.address1}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'السعر: ${apartment.price}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {},
                               ),
-                              TextButton(
-                                child: Text('Delete'),
+                              IconButton(
+                                icon: Icon(Icons.delete),
                                 onPressed: () {
-                                  // Delete the apartment from Firebase and the list
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text('تأكيد الحذف'),
+                                        content: Text(
+                                          'هل أنت متأكد أنك تريد حذف هذه الشقة؟',
+                                          textAlign: TextAlign.right,
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            child: Text('إلغاء'),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                          TextButton(
+                                            child: Text('حذف'),
+                                            onPressed: () {
+                                              // Delete the apartment from Firebase and the list
 
-                                  Navigator.of(context).pop();
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
                                 },
                               ),
                             ],
-                          );
-                        },
-                      );
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () {},
-                  ),
-                ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
               ),
-            ),
-          );
-        },
       ),
     );
   }
