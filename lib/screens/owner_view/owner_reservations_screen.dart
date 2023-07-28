38525150c468b529d41501e72442d080b56f32ec
eman khalaf <eman.mufeed.khak@ninjavan.co>
2023-07-28 17:10:24 +0300

    reservationa and fix report a problem

--- lib/screens/owner_view/owner_reservations_screen.dart
+++ lib/screens/owner_view/owner_reservations_screen.dart
@@ -6,16 +6,22 @@ import '../Main/main_screen.dart';
 import 'owner_view_screen.dart';
 
 class Reserves {
+  final String reservationId;
   final String apartmentcity;
   final String customerName;
   final String date;
   final String time;
+  bool isPending;
+  bool isApproved;
 
   Reserves({
+    required this.reservationId,
     required this.apartmentcity,
     required this.customerName,
     required this.date,
     required this.time,
+    this.isApproved = false,
+    this.isPending = true,
   });
 }
 
@@ -32,6 +38,7 @@ class OwnerReservationsScreen extends StatefulWidget {
 class _OwnerReservationsScreenState extends State<OwnerReservationsScreen> {
   late DatabaseReference _databaseRef;
   List<Reserves> _reserves = [];
+
   @override
   void initState() {
     super.initState();
@@ -47,14 +54,32 @@ class _OwnerReservationsScreenState extends State<OwnerReservationsScreen> {
               .map((entry) {
             final reservation = Map<String, dynamic>.from(entry.value);
             return Reserves(
+              reservationId:
+                  entry.key, // Pass the reservationId to the constructor
               apartmentcity: reservation['apartmentcity'] as String,
               customerName: reservation['customerName'] as String,
               date: reservation['date'] as String,
               time: reservation['time'] as String,
+              isApproved: reservation['isApproved'] as bool,
             );
           }).toList();
         });
       }
+    });
+  }
+
+  // Function to update the approval status in Firebase
+  void updateReservationApprovalStatus(String reservationId, bool isApproved) {
+    DatabaseReference reservationRef =
+        FirebaseDatabase.instance.reference().child('reservations');
+
+    reservationRef.child(reservationId).update({
+      'isApproved': isApproved,
+      'isPending': false,
+    }).then((_) {
+      // Do something after successful update, if needed.
+    }).catchError((error) {
+      print('Error updating approval status: $error');
     });
   }
 
@@ -87,9 +112,9 @@ class _OwnerReservationsScreenState extends State<OwnerReservationsScreen> {
                 itemBuilder: (BuildContext context, int index) {
                   final reservation = _reserves[index];
                   return Card(
-                    elevation: 4, // Add elevation for a subtle shadow
+                    elevation: 4,
                     margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    color: Colors.grey[100], // Change background color
+                    color: Colors.grey[100],
                     child: Padding(
                       padding: EdgeInsets.all(16),
                       child: Directionality(
@@ -123,6 +148,83 @@ class _OwnerReservationsScreenState extends State<OwnerReservationsScreen> {
                               style: TextStyle(fontSize: 14),
                               textAlign: TextAlign.right,
                             ),
+                            SizedBox(height: 16),
+                            if (reservation.isApproved)
+                              Text(
+                                'تم الموافقة على الحجز',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                                textAlign: TextAlign.center,
+                              )
+                            else if (reservation.isPending)
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.green,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        _reserves[index].isApproved = true;
+                                      });
+                                      updateReservationApprovalStatus(
+                                          reservation.reservationId,
+                                          true); // Update approval status in Firebase
+                                    },
+                                    child: Text(
+                                      'قبول',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.red,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        _reserves[index].isApproved = false;
+                                        _reserves[index].isPending =
+                                            false; // Set isPending to false when rejecting the reservation
+                                      });
+                                      updateReservationApprovalStatus(
+                                        reservation.reservationId,
+                                        false,
+                                      ); // Update approval status in Firebase
+                                    },
+                                    child: Text(
+                                      'رفض',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            else
+                              Text(
+                                'لقد قمت برفض الحجز',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
                           ],
                         ),
                       ),
